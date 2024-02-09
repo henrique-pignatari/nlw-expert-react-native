43499037d350313dcbc0b48ddb21d7ccfdfca612
henrique-pignatari <henrique.pignatarimoraes@gmail.com>
2024-02-09 00:15:29 -0300

    feat: add car persistence

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,5 +1,8 @@
+import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
 import { ProductProps } from "@/utils/data/products"
-import {create} from "zustand"
 import * as cartInMemory from "./helpers/cart-in-memory"
 
 export type ProductCartProps = ProductProps & {
@@ -12,7 +15,8 @@ type StateProps = {
     remove: (productId: string) => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+    persist<StateProps>((set) => ({
     products: [],
     add: (product: ProductProps) => set((state) => ({
         products: cartInMemory.add(state.products, product)
@@ -20,4 +24,7 @@ export const useCartStore = create<StateProps>((set) => ({
     remove: (productId: string) => set((state) => ({
         products: cartInMemory.remove(state.products, productId)
     }))
+}),{
+    name: "nlw-expert:cart",
+    storage: createJSONStorage(() => AsyncStorage)
 }))
